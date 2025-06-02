1ead61b179fb486d2b48d1ec95b818b87b623f79
Shouq <shouqalrumaih@gmail.com>
2025-06-02 18:37:29 +0300

    completed SVM lesson

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,10 +12,13 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tools')))
+
 from email_preprocess import preprocess
 
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -25,23 +28,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ##############################################################
 # Enter Your Code Here
 
+model = GaussianNB()
+
+t0 = time()
+model.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+actual_pred = model.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(labels_test , actual_pred )
+print("accuracy: ", accuracy)
 
 
-##############################################################
-
-##############################################################
-'''
-You Will be Required to record time for Training and Predicting 
-The Code Given on Udacity Website is in Python-2
-The Following Code is Python-3 version of the same code
-'''
-
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
-
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
-
-##############################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,30 +10,33 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tools')))
 from email_preprocess import preprocess
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
+
+model = SVC(kernel= "rbf", C= 10000)
+
+t0 = time()
+model.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = model.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(labels_test , pred)
+
+print("accuracy: " , accuracy)
+
+chris_count = list(pred).count(1)
+print("Number of predicted Chris emails:", chris_count)
 
 
-#########################################################
-### your code goes here ###
 
-
-#########################################################
-
-#########################################################
-'''
-You'll be Provided similar code in the Quiz
-But the Code provided in Quiz has an Indexing issue
-The Code Below solves that issue, So use this one
-'''
-
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
-
-#########################################################
